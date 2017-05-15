fc05bf55d0b51fea03c8ebadea2960eab808e257
shaddu <shadab.ahmed63@gmail.com>
2017-05-15 23:50:34 +0530

    bot bot

--- service.py
+++ service.py
@@ -3,6 +3,9 @@
 
 def handler(event, context):
     # Your code goes here!
-    e = event.get('e')
-    pi = event.get('pi')
-    return e + pi
+    # e = event.get('e')
+    # pi = event.get('pi')
+    # return e + pi
+    result = event['currentIntent']['slots']['city']
+    # + " is highly polluted"
+    return result
